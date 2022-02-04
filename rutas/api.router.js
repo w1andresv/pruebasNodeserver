475b7804174c5f792915b4e56c7f2175e67a441e
weymar <weymar.vargas@eventsite.com.co>
2022-02-04 09:53:16 -0500

    aws6

--- rutas/api.router.js
+++ rutas/api.router.js
@@ -22,7 +22,7 @@ app.get('/connection', (req, res) => {
  console.log('---------CONECTADO-----------');
  console.log(req);
   // console.log(req);
-  res.status(200).json({ estado: true, token: 'token' });
+  // res.status(200).json({ estado: true, token: 'token' });
 
 
 
