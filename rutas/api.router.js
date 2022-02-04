084eafc4d7d3014d5f2c2d3ffe048a1a3fe4dd3c
weymar <weymar.vargas@eventsite.com.co>
2022-02-04 09:36:25 -0500

    AWS4

--- rutas/api.router.js
+++ rutas/api.router.js
@@ -5,12 +5,21 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+const ENDPOINT = 'c49ab8hu8d.execute-api.us-west-2.amazonaws.com/desarrollo/';
+const client = new AWS.ApiGatewayManagementApi({ endpoint: ENDPOINT });
 app.get('/', (req, res) => {
 
   const ENDPOINT = 'c49ab8hu8d.execute-api.us-west-2.amazonaws.com/desarrollo/';
   const client = new AWS.ApiGatewayManagementApi({ endpoint: ENDPOINT });
   console.log(client);
+  // console.log(req);
+  res.status(200).json({ estado: true, token: 'token' });
+
+
+
+});
+app.get('/connection', (req, res) => {
+ console.log('---------CONECTADO-----------');
   // console.log(req);
   res.status(200).json({ estado: true, token: 'token' });
 
